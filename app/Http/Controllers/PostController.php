5eb02a56d09b08bad8cdf84833aed15cc11577f9
Edgar Estigarribia <edgarestigarribia@hotmail.com>
2023-05-30 16:29:00 -0400

    Muro

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 
@@ -12,8 +13,16 @@ class PostController extends Controller
     {
         $this->middleware('auth');
     }
-    public function index()
+    public function index(User $user)
     {
-        return view('dashboard');
+        //dd($user->username);
+        return view('dashboard', [
+            'user' => $user
+        ]);
+    }
+
+    public function create()
+    {
+        dd('Creando post...');
     }
 }
