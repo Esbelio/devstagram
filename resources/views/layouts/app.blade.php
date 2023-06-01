6adccaef8bf42431016f06f18897f880ce4ca51a
Edgar Estigarribia <edgarestigarribia@hotmail.com>
2023-06-01 15:53:17 -0400

    Dropzone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
         @vite('resources/css/app.css')
        
-    </head>
+    </head> 
     <body class="bg-gray-100">
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
