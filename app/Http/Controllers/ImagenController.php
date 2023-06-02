bb141019d4befb56cb263c65bcb7b2164178c59a
esbelio <edgarestigarribia@gmail.com>
2023-06-02 15:02:03 -0400

    .

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     //
     public function store(Request $request)
     {
-        $imagene = $request->file('file');
+        $imagen = $request->file('file');
 
         $nombreImagen = Str::uuid() . "." .$imagen->extension();
 
-        return response()->json(['imagen' => $imagene->extension()]);
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000,1000);
+
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
