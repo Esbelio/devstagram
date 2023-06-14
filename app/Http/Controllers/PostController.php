6c23c488cc26ad5c763ae6e76c89252517c5407e
esbelio <edgarestigarribia@gmail.com>
2023-06-13 23:27:15 -0400

    Eliminar imagenes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -77,6 +77,12 @@ class PostController extends Controller
     {
         $this->authorize('delete', $post);
         $post->delete();
+
+        //Eliminar la imagen
+        $imagen_path = public_path('uploads/' . $post->imagen);
+        if (File::exists($imagen_path)) {
+            unlink($imagen_path);
+        }
 
         return redirect()->route('post.index', auth()->user()->username);
     }
